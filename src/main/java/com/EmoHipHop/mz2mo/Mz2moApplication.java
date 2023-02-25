26654168fd02eba3b6479d469fd15cae4ff6acf0
chxxyx <codud0826@naver.com>
2023-02-25 15:12:35 +0900

    Initial commit

---
+++ src/main/java/com/EmoHipHop/mz2mo/Mz2moApplication.java
@@ -0,0 +1,13 @@
+package com.EmoHipHop.mz2mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mz2moApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mz2moApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmoHipHop/mz2mo/Mz2moApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmoHipHop.mz2mo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mz2moApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

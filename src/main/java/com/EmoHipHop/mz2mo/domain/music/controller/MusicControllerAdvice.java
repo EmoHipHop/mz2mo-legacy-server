b00c2600f2a56983c965d75880aa276ea1cc7556
devel <develop.raul@gmail.com>
2023-02-26 03:21:58 +0900

    add: MusicControllerAdvice 작성 및 이에 필요한 컴포넌트 작성

---
+++ src/main/java/com/EmoHipHop/mz2mo/domain/music/controller/MusicControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.EmoHipHop.mz2mo.domain.music.controller;
+
+import com.EmoHipHop.mz2mo.domain.music.exception.DuplicateSpotifyUriException;
+import com.EmoHipHop.mz2mo.domain.music.exception.MalformedSpotifyUriException;
+import com.EmoHipHop.mz2mo.global.common.data.response.ErrorResponse;
+import com.EmoHipHop.mz2mo.global.common.data.type.ErrorCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MusicControllerAdvice {
+    @ExceptionHandler(MalformedSpotifyUriException.class)
+    public ResponseEntity<ErrorResponse> handleMalformedSpotifyUriException(MalformedSpotifyUriException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(e.getMessage(), ErrorCode.MALFORMED_SPOTIFY_URI));
+    }
+
+    @ExceptionHandler(DuplicateSpotifyUriException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateSpotifyUriException(DuplicateSpotifyUriException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(e.getMessage(), ErrorCode.DUPLICATE_SPOTIFY_URI));
+    }
+}
---
+++ src/main/java/com/EmoHipHop/mz2mo/global/common/data/type/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.EmoHipHop.mz2mo.global.common.data.type;
+
+public enum ErrorCode {
+    MALFORMED_SPOTIFY_URI, DUPLICATE_SPOTIFY_URI
+}

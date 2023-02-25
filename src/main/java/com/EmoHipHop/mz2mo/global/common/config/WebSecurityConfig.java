b8a72bdb5292a1ab2e26611c0c5ca57035022f7e
devel <develop.raul@gmail.com>
2023-02-26 03:12:13 +0900

    fix: 패키지 구조 변경

--- src/main/java/com/EmoHipHop/mz2mo/global/common/config/WebSecurityConfig.java
+++ src/main/java/com/EmoHipHop/mz2mo/global/common/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.EmoHipHop.mz2mo.global.config;
+package com.EmoHipHop.mz2mo.global.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

814ef19b2796a88ac9a06766f0f6180002f5ffa5
devel <develop.raul@gmail.com>
2023-02-26 17:25:57 +0900

    fix: RemoveVoteResponse 스팩 수정 및 VoteConverter 구현체에 기능추가/수정사항 반영

--- src/main/java/com/EmoHipHop/mz2mo/domain/vote/data/response/RemoveVoteResponse.java
+++ src/main/java/com/EmoHipHop/mz2mo/domain/vote/data/response/RemoveVoteResponse.java
@@ -1,4 +1,3 @@
 package com.EmoHipHop.mz2mo.domain.vote.data.response;
 
-public record RemoveVoteResponse() {
-}
+public record RemoveVoteResponse(String userId, String musicId, String[] currentVoteEmojiCodes) { }
--- src/main/java/com/EmoHipHop/mz2mo/domain/vote/util/VoteConverterImpl.java
+++ src/main/java/com/EmoHipHop/mz2mo/domain/vote/util/VoteConverterImpl.java
@@ -2,10 +2,13 @@ package com.EmoHipHop.mz2mo.domain.vote.util;
 
 import com.EmoHipHop.mz2mo.domain.emoji.repository.EmojiRepository;
 import com.EmoHipHop.mz2mo.domain.vote.data.dto.AddVoteDto;
+import com.EmoHipHop.mz2mo.domain.vote.data.dto.RemoveVoteDto;
 import com.EmoHipHop.mz2mo.domain.vote.data.dto.VoteDto;
 import com.EmoHipHop.mz2mo.domain.vote.data.entity.MusicEmojiVote;
 import com.EmoHipHop.mz2mo.domain.vote.data.request.AddVoteRequest;
+import com.EmoHipHop.mz2mo.domain.vote.data.request.RemoveVoteRequest;
 import com.EmoHipHop.mz2mo.domain.vote.data.response.AddVoteResponse;
+import com.EmoHipHop.mz2mo.domain.vote.data.response.RemoveVoteResponse;
 import com.EmoHipHop.mz2mo.global.emoji.data.entity.Emoji;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -27,6 +30,15 @@ public class VoteConverterImpl implements VoteConverter {
     }
 
     @Override
+    public RemoveVoteDto toDto(RemoveVoteRequest request, String userId, String musicId) {
+        return new RemoveVoteDto(
+                userId,
+                musicId,
+                emojiRepository.findByCode(request.emojiCode()).getId()
+        );
+    }
+
+    @Override
     public VoteDto toDto(List<MusicEmojiVote> votes) {
         MusicEmojiVote vote = votes.get(0);
         return new VoteDto(
@@ -38,11 +50,20 @@ public class VoteConverterImpl implements VoteConverter {
     }
 
     @Override
-    public AddVoteResponse toResponse(VoteDto dto) {
+    public AddVoteResponse toAddResponse(VoteDto dto) {
         return new AddVoteResponse(
             dto.userId(),
             dto.musicId(),
             dto.currentVoteEmojiIds()
+        );
+    }
+
+    @Override
+    public RemoveVoteResponse toRemoveResponse(VoteDto dto) {
+        return new RemoveVoteResponse(
+                dto.userId(),
+                dto.musicId(),
+                dto.currentVoteEmojiIds()
         );
     }
 }

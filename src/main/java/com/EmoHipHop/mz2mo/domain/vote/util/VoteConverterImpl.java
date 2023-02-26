3e539fe9b1ebcaf9020bbb0721757c93c4784835
devel <develop.raul@gmail.com>
2023-02-26 14:56:21 +0900

    add: VoteConverter 추가스팩 구현

--- src/main/java/com/EmoHipHop/mz2mo/domain/vote/util/VoteConverterImpl.java
+++ src/main/java/com/EmoHipHop/mz2mo/domain/vote/util/VoteConverterImpl.java
@@ -2,13 +2,16 @@ package com.EmoHipHop.mz2mo.domain.vote.util;
 
 import com.EmoHipHop.mz2mo.domain.vote.data.dto.AddVoteDto;
 import com.EmoHipHop.mz2mo.domain.vote.data.dto.VoteDto;
+import com.EmoHipHop.mz2mo.domain.vote.data.entity.MusicEmojiVote;
 import com.EmoHipHop.mz2mo.domain.vote.data.request.AddVoteRequest;
 import com.EmoHipHop.mz2mo.domain.vote.data.response.AddVoteResponse;
+import com.EmoHipHop.mz2mo.global.emoji.data.entity.Emoji;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class VoteConverterImpl implements VoteConverter {
-
     @Override
     public AddVoteDto toDto(AddVoteRequest request, String userId, String musicId) {
         return new AddVoteDto(
@@ -19,11 +22,22 @@ public class VoteConverterImpl implements VoteConverter {
     }
 
     @Override
+    public VoteDto toDto(List<MusicEmojiVote> votes) {
+        MusicEmojiVote vote = votes.get(0);
+        return new VoteDto(
+                vote.getId(),
+                vote.getUser().getId(),
+                vote.getMusic().getId(),
+                votes.stream().map(MusicEmojiVote::getEmoji).map(Emoji::getId).toArray(String[]::new)
+        );
+    }
+
+    @Override
     public AddVoteResponse toResponse(VoteDto dto) {
         return new AddVoteResponse(
             dto.userId(),
             dto.musicId(),
-            dto.currentVoteEmojiCodes()
+            dto.currentVoteEmojiIds()
         );
     }
 }

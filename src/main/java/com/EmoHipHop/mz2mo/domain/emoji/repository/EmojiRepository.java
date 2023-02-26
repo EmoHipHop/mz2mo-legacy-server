cab54bef85b687c24b4bc64d0d58d84d8cf4355a
devel <develop.raul@gmail.com>
2023-02-26 11:46:36 +0900

    add: EmojiRepository 및 관련 엔티티 작성

---
+++ src/main/java/com/EmoHipHop/mz2mo/domain/emoji/repository/EmojiRepository.java
@@ -0,0 +1,7 @@
+package com.EmoHipHop.mz2mo.domain.emoji.repository;
+
+import com.EmoHipHop.mz2mo.domain.emoji.data.entity.Emoji;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmojiRepository extends JpaRepository<Emoji, String> {
+}
